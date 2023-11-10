8c3dbfb89420cae85de60c3f341a2ba1bd27a1b8
enyllaoliveira <enyllaoliveira@gmail.com>
2023-11-10 18:08:34 -0300

    starting first NextJs Page

---
+++ nextjs_project/src/components/basics/first.tsx
@@ -0,0 +1,5 @@
+export default function PageOne() {
+    return (
+        <div> Primeiro Componente</div>
+    )
+}
---
+++ nextjs_project/src/pages/basic/first.tsx
@@ -0,0 +1,12 @@
+import PageOne from "@/components/basics/first"
+
+export default function FirstPage() {
+    return (
+        <div>
+
+        <PageOne/>  
+        <PageOne/>  
+        </div>
+
+    )
+}
